e2d4c01782185098cf35887260ccceac2baa99a4
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2018-08-28 21:36:51 +0200

    rename class

--- src/AWSLambdaInfo/AWSLambdaInfo/Assemblies.cs
+++ src/AWSLambdaInfo/AWSLambdaInfo/Assemblies.cs
@@ -2,25 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
-
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
+using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace AWSLambdaInfo
 {
-    public class Functions
+    public class Assemblies
     {
         /// <summary>
         /// Default constructor that Lambda will invoke.
         /// </summary>
-        public Functions()
+        public Assemblies()
         {
         }
-
 
         /// <summary>
         /// A Lambda function to respond to HTTP Get methods from API Gateway
@@ -29,13 +27,16 @@ namespace AWSLambdaInfo
         /// <returns>The list of blogs</returns>
         public APIGatewayProxyResponse Get(APIGatewayProxyRequest request, ILambdaContext context)
         {
-            context.Logger.LogLine("Get Request\n");
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var result = assemblies.Except(new[] { typeof(Assemblies).Assembly }).OrderBy(x => x.FullName).Select(x => x.ToString()).ToList();
+
+            context.Logger.LogLine($"{assemblies.Length} assemblies found");
 
             var response = new APIGatewayProxyResponse
             {
                 StatusCode = (int)HttpStatusCode.OK,
-                Body = "Hello AWS Serverless",
-                Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
+                Body = JsonConvert.SerializeObject(result),
+                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
             };
 
             return response;
