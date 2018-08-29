7d5c4f94fd3d491b9704650b8de3d6e7ff5ea699
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2018-08-29 21:47:37 +0200

    clean

--- src/AWSLambdaInfo/AWSLambdaInfo/Assemblies.cs
+++ src/AWSLambdaInfo/AWSLambdaInfo/Assemblies.cs
@@ -6,25 +6,12 @@ using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
 using Newtonsoft.Json;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace AWSLambdaInfo
 {
     public class Assemblies
     {
-        /// <summary>
-        /// Default constructor that Lambda will invoke.
-        /// </summary>
-        public Assemblies()
-        {
-        }
-
-        /// <summary>
-        /// A Lambda function to respond to HTTP Get methods from API Gateway
-        /// </summary>
-        /// <param name="request"></param>
-        /// <returns>The list of blogs</returns>
         public APIGatewayProxyResponse Get(APIGatewayProxyRequest request, ILambdaContext context)
         {
             var assemblies = AppDomain.CurrentDomain.GetAssemblies();
@@ -32,14 +19,12 @@ namespace AWSLambdaInfo
 
             context.Logger.LogLine($"{assemblies.Length} assemblies found");
 
-            var response = new APIGatewayProxyResponse
+            return new APIGatewayProxyResponse
             {
                 StatusCode = (int)HttpStatusCode.OK,
                 Body = JsonConvert.SerializeObject(result),
                 Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
             };
-
-            return response;
         }
     }
 }
