dcf9c4315bf3b4a2e11b5a5a6a508aac6d5157f6
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2023-02-05 09:45:47 +0100

    :arrow_up:

--- src/AWSLambdaInfo/Assemblies.cs
+++ src/AWSLambdaInfo/Assemblies.cs
@@ -1,30 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
 using System.Net;
 using System.Text.Json;
-using Amazon.Lambda.Core;
-using Amazon.Lambda.APIGatewayEvents;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace AWSLambdaInfo
+namespace AWSLambdaInfo;
+
+public class Assemblies
 {
-    public class Assemblies
+    public APIGatewayProxyResponse Get(APIGatewayProxyRequest request, ILambdaContext context)
     {
-        public APIGatewayProxyResponse Get(APIGatewayProxyRequest request, ILambdaContext context)
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        var result = assemblies.Except(new[] { typeof(Assemblies).Assembly }).OrderBy(x => x.FullName).Select(x => x.ToString()).ToList();
+
+        context.Logger.LogLine($"{assemblies.Length} assemblies found");
+
+        return new APIGatewayProxyResponse
         {
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            var result = assemblies.Except(new[] { typeof(Assemblies).Assembly }).OrderBy(x => x.FullName).Select(x => x.ToString()).ToList();
-
-            context.Logger.LogLine($"{assemblies.Length} assemblies found");
-
-            return new APIGatewayProxyResponse
-            {
-                StatusCode = (int)HttpStatusCode.OK,
-                Body = JsonSerializer.Serialize(result),
-                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-            };
-        }
+            StatusCode = (int)HttpStatusCode.OK,
+            Body = JsonSerializer.Serialize(result),
+            Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+        };
     }
 }
--- src/AWSLambdaInfo/Type.cs
+++ src/AWSLambdaInfo/Type.cs
@@ -1,97 +1,93 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text.Json;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using System.Net;
+using System.Text.Json;
 
-namespace AWSLambdaInfo
+namespace AWSLambdaInfo;
+
+public class Type
 {
-    public class Type
+    public APIGatewayProxyResponse Get(APIGatewayProxyRequest request, ILambdaContext context)
     {
-        public APIGatewayProxyResponse Get(APIGatewayProxyRequest request, ILambdaContext context)
-        {
-            var fullName = request.QueryStringParameters.FirstOrDefault(q => string.Compare(q.Key, "FullName", StringComparison.OrdinalIgnoreCase) == 0).Value;
-            var name = request.QueryStringParameters.FirstOrDefault(q => string.Compare(q.Key, "Name", StringComparison.OrdinalIgnoreCase) == 0).Value;
+        var fullName = request.QueryStringParameters.FirstOrDefault(q => string.Compare(q.Key, "FullName", StringComparison.OrdinalIgnoreCase) == 0).Value;
+        var name = request.QueryStringParameters.FirstOrDefault(q => string.Compare(q.Key, "Name", StringComparison.OrdinalIgnoreCase) == 0).Value;
 
-            if (!string.IsNullOrEmpty(fullName)) return ByFullName(fullName, context);
+        if (!string.IsNullOrEmpty(fullName)) return ByFullName(fullName, context);
 
-            if (!string.IsNullOrEmpty(name)) return ByName(name, context);
+        if (!string.IsNullOrEmpty(name)) return ByName(name, context);
 
-            return HttpStatusCode.BadRequest.ToResponse();
-        }
-
-        private static APIGatewayProxyResponse ByFullName(string fullName, ILambdaContext context)
-        {
-            var result = AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(x => x.GetTypes())
-                .SkipExceptions()
-                .Where(x => x.FullName == fullName)
-                .OrderBy(x => x.Assembly.FullName)
-                .Select(x => x.Assembly.ToString())
-                .ToList();
-
-            context.Logger.LogLine($"{fullName} was found in {result.Count} assemblies");
-
-            if (result.Any()) return result.ToOkResponse();
-
-            return HttpStatusCode.NotFound.ToResponse();
-        }
-
-        private static APIGatewayProxyResponse ByName(string name, ILambdaContext context)
-        {
-            var result = AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(x => x.GetTypes())
-                .SkipExceptions()
-                .Where(x => x.Name == name)
-                .OrderBy(x => x.Assembly.FullName)
-                .Select(x => new { Type = x.FullName, Assembly = x.Assembly.ToString() })
-                .ToList();
-
-            context.Logger.LogLine($"{name} matched {result.Count} types");
-
-            if (result.Any()) return result.ToOkResponse();
-
-            return HttpStatusCode.NotFound.ToResponse();
-        }
+        return HttpStatusCode.BadRequest.ToResponse();
     }
 
-    internal static class Extensions
+    private static APIGatewayProxyResponse ByFullName(string fullName, ILambdaContext context)
     {
-        internal static APIGatewayProxyResponse ToOkResponse(this object result)
-        {
-            return new APIGatewayProxyResponse
-            {
-                StatusCode = (int)HttpStatusCode.OK,
-                Body = JsonSerializer.Serialize(result),
-                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-            };
-        }
+        var result = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(x => x.GetTypes())
+            .SkipExceptions()
+            .Where(x => x.FullName == fullName)
+            .OrderBy(x => x.Assembly.FullName)
+            .Select(x => x.Assembly.ToString())
+            .ToList();
 
-        internal static APIGatewayProxyResponse ToResponse(this HttpStatusCode statusCode)
-        {
-            return new APIGatewayProxyResponse { StatusCode = (int)statusCode };
-        }
+        context.Logger.LogLine($"{fullName} was found in {result.Count} assemblies");
 
-        internal static IEnumerable<T> SkipExceptions<T>(this IEnumerable<T> values)
+        if (result.Any()) return result.ToOkResponse();
+
+        return HttpStatusCode.NotFound.ToResponse();
+    }
+
+    private static APIGatewayProxyResponse ByName(string name, ILambdaContext context)
+    {
+        var result = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(x => x.GetTypes())
+            .SkipExceptions()
+            .Where(x => x.Name == name)
+            .OrderBy(x => x.Assembly.FullName)
+            .Select(x => new { Type = x.FullName, Assembly = x.Assembly.ToString() })
+            .ToList();
+
+        context.Logger.LogLine($"{name} matched {result.Count} types");
+
+        if (result.Any()) return result.ToOkResponse();
+
+        return HttpStatusCode.NotFound.ToResponse();
+    }
+}
+
+internal static class Extensions
+{
+    internal static APIGatewayProxyResponse ToOkResponse(this object result)
+    {
+        return new APIGatewayProxyResponse
         {
-            using (var enumerator = values.GetEnumerator())
+            StatusCode = (int)HttpStatusCode.OK,
+            Body = JsonSerializer.Serialize(result),
+            Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+        };
+    }
+
+    internal static APIGatewayProxyResponse ToResponse(this HttpStatusCode statusCode)
+    {
+        return new APIGatewayProxyResponse { StatusCode = (int)statusCode };
+    }
+
+    internal static IEnumerable<T> SkipExceptions<T>(this IEnumerable<T> values)
+    {
+        using (var enumerator = values.GetEnumerator())
+        {
+            var next = true;
+            while (next)
             {
-                var next = true;
-                while (next)
+                try
                 {
-                    try
-                    {
-                        next = enumerator.MoveNext();
-                    }
-                    catch
-                    {
-                        continue;
-                    }
-
-                    if (next) yield return enumerator.Current;
+                    next = enumerator.MoveNext();
                 }
+                catch
+                {
+                    continue;
+                }
+
+                if (next) yield return enumerator.Current;
             }
         }
     }
